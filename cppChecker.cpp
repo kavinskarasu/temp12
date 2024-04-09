6041acb676bc1c208e62c4a23907101e1155bc14
KAVIARASU S <kavinskarasu@gmail.com>
2024-04-09 11:32:08 +0530

    updated

--- cppChecker.cpp
+++ cppChecker.cpp
@@ -6,19 +6,19 @@
 
 
 
-// BOOL CreateProcessAsUserA(
-//   [in, optional]      HANDLE                hToken,
-//   [in, optional]      LPCSTR                lpApplicationName,
-//   [in, out, optional] LPSTR                 lpCommandLine,
-//   [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
-//   [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
-//   [in]                BOOL                  bInheritHandles,
-//   [in]                DWORD                 dwCreationFlags,
-//   [in, optional]      LPVOID                lpEnvironment,
-//   [in, optional]      LPCSTR                lpCurrentDirectory,
-//   [in]                LPSTARTUPINFOA        lpStartupInfo,
-//   [out]               LPPROCESS_INFORMATION lpProcessInformation
-// );
+BOOL CreateProcessAsUserA(
+  [in, optional]      HANDLE                hToken,
+  [in, optional]      LPCSTR                lpApplicationName,
+  [in, out, optional] LPSTR                 lpCommandLine,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
+  [in]                BOOL                  bInheritHandles,
+  [in]                DWORD                 dwCreationFlags,
+  [in, optional]      LPVOID                lpEnvironment,
+  [in, optional]      LPCSTR                lpCurrentDirectory,
+  [in]                LPSTARTUPINFOA        lpStartupInfo,
+  [out]               LPPROCESS_INFORMATION lpProcessInformation
+);
 
 // BOOL CreateProcessAsUserW                                      (
 //   [in, optional]      HANDLE                hToken,
