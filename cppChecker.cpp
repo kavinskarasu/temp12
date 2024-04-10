148feb71e01990f1295529f138aa320e7eff17d5
kavinskarasu <kavinskarasu@gmail.com>
2024-04-10 14:13:53 +0530

    updaed

--- cppChecker.cpp
+++ cppChecker.cpp
@@ -6,37 +6,37 @@
 
 
 
-BOOL CreateProcessAsUserA(
-  [in, optional]      HANDLE                hToken,
-  [in, optional]      LPCSTR                lpApplicationName,
-  [in, out, optional] LPSTR                 lpCommandLine,
-  [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
-  [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
-  [in]                BOOL                  bInheritHandles,
-  [in]                DWORD                 dwCreationFlags,
-  [in, optional]      LPVOID                lpEnvironment,
-  [in, optional]      LPCSTR                lpCurrentDirectory,
-  [in]                LPSTARTUPINFOA        lpStartupInfo,
-  [out]               LPPROCESS_INFORMATION lpProcessInformation
-);
+// BOOL CreateProcessAsUserA(
+//   [in, optional]      HANDLE                hToken,
+//   [in, optional]      LPCSTR                lpApplicationName,
+//   [in, out, optional] LPSTR                 lpCommandLine,
+//   [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
+//   [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
+//   [in]                BOOL                  bInheritHandles,
+//   [in]                DWORD                 dwCreationFlags,
+//   [in, optional]      LPVOID                lpEnvironment,
+//   [in, optional]      LPCSTR                lpCurrentDirectory,
+//   [in]                LPSTARTUPINFOA        lpStartupInfo,
+//   [out]               LPPROCESS_INFORMATION lpProcessInformation
+// );
 
- BOOL CreateProcessAsUserW                                      (
-  [in, optional]      HANDLE                hToken,
-  [in, optional]      LPCWSTR               lpApplicationName,
-  [in, out, optional] LPWSTR                lpCommandLine,
-  [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
-  [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
-  [in]                BOOL                  bInheritHandles,
-  [in]                DWORD                 dwCreationFlags,
-  [in, optional]      LPVOID                lpEnvironment,
-  [in, optional]      LPCWSTR               lpCurrentDirectory,
-  [in]                LPSTARTUPINFOW        lpStartupInfo,
-  [out]               LPPROCESS_INFORMATION lpProcessInformation
-);
+//  BOOL CreateProcessAsUserW                                      (
+//   [in, optional]      HANDLE                hToken,
+//   [in, optional]      LPCWSTR               lpApplicationName,
+//   [in, out, optional] LPWSTR                lpCommandLine,
+//   [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
+//   [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
+//   [in]                BOOL                  bInheritHandles,
+//   [in]                DWORD                 dwCreationFlags,
+//   [in, optional]      LPVOID                lpEnvironment,
+//   [in, optional]      LPCWSTR               lpCurrentDirectory,
+//   [in]                LPSTARTUPINFOW        lpStartupInfo,
+//   [out]               LPPROCESS_INFORMATION lpProcessInformation
+// );
  
 
  
- #include <iostream>
+  #include <iostream>
 #include <cstring>
 using namespace std;
 
